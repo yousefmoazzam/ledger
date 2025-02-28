5b05c7caf3fa86a807a927f2bc4ea46f9bb2e87c
Yousef Moazzam <yousefmoazzam@hotmail.co.uk>
2025-02-28 21:48:41 +0000

    Use "script sig" and "script sig size" in transaction input
    Instead of trying to provide some extra types to express the constraint
    of using only P2PKH, keep `Input` simple for now and express the
    constraints of P2PKH and ECDSA signatures where appropriate in the docs.

--- src/transaction.rs
+++ src/transaction.rs
@@ -1,10 +1,5 @@
-/// Unlocking of "pay to public key hash" script pattern
-struct P2PKHUnlock {
-    /// Public key of owner of funds
-    public_key: Vec<u8>,
-    /// Signature verifying owner of funds
-    signature: Vec<u8>,
-}
+const SHA256_OUT_SIZE: u8 = 32;
+const ECDSA_SIG_SIZE: u8 = 64;
 
 /// Locking performed by "pay to public key hash" script pattern
 struct P2PKHLock {
@@ -13,15 +8,31 @@ struct P2PKHLock {
 }
 
 /// Transaction input
+///
+/// Note: Assume the use of "pay to public key hash" (P2PKH) script pattern and uncompressed ECDSA
+/// keys
 pub(crate) struct Input {
     /// ID of transaction to spend from
     transaction_id: Vec<u8>,
     /// Index of output of transaction to spend from
     output_index: u32,
-    /// Data required for unlocking transaction
-    unlock_mechanism: P2PKHUnlock,
+    /// Data for unlocking referenced transaction
+    ///
+    /// Note: From P2PKH and ECDSA key assumption, plus not including things related to the Script
+    /// language, this contains just the SHA256 hash of the public key of the owner of the
+    /// referenced transaction, and the ECDSA signature of the transaction which is using this
+    /// input
+    script_sig: Vec<u8>,
     /// Sequence number of transaction
     sequence: u32,
+}
+
+impl Input {
+    /// Size of `script_sig` field
+    ///
+    /// Note: Assuming `P2PKH` script pattern and ECDSA signature means that the length is always
+    /// the same and is thus compile-time known for every instance of [`Input`]
+    const SCRIPT_SIG_SIZE: u8 = SHA256_OUT_SIZE + ECDSA_SIG_SIZE;
 }
 
 /// Transaction output
